0058036096a006793f77d106b9e5e43fcb2250a5
長政輝 <>
2021-05-25 14:57:17 +0900

    [add] components/icon_button

---
+++ lib/components/icon_button.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class IconButtonWidget extends StatelessWidget {
+  final Icon icon;
+  final String text;
+  final Color color;
+  IconButtonWidget({
+    @required this.icon,
+    @required this.text,
+    @required this.color,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton.icon(
+      style: ElevatedButton.styleFrom(
+        minimumSize: Size(300, 50),
+        primary: color,
+        shape: StadiumBorder(),
+      ),
+      icon: icon,
+      label: Text(
+        text,
+        style: TextStyle(fontSize: 17),
+      ),
+      onPressed: () {},
+    );
+  }
+}
