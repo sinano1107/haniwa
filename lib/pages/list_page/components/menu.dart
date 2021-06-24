eafd6d08c037d49af61d57bf9348a6f1b36b550f
長政輝 <>
2021-06-24 18:54:46 +0900

    [update] menuに所持ポイントを表示

--- lib/pages/list_page/components/menu.dart
+++ lib/pages/list_page/components/menu.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:haniwa/common/firestore.dart';
 import 'package:haniwa/common/progress.dart';
 import 'package:haniwa/common/snackbar.dart';
+import 'package:haniwa/models/member.dart';
 import 'package:haniwa/pages/signin_page/index.dart';
 
 class Menu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final _theme = Theme.of(context);
+    final _user = FirebaseAuth.instance.currentUser;
+
     return SafeArea(
       bottom: false,
       child: ClipRRect(
@@ -14,10 +19,31 @@ class Menu extends StatelessWidget {
         child: Drawer(
           child: ListView(
             children: [
-              DrawerHeader(
-                child: Container(),
-                decoration: BoxDecoration(
-                  color: Colors.blue,
+              UserAccountsDrawerHeader(
+                decoration: BoxDecoration(color: Colors.cyan),
+                accountName: Text('${_user.displayName} の所持ポイント'),
+                accountEmail: FutureBuilder<Member>(
+                  future: fetchMemberData(_user.uid),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState != ConnectionState.done) {
+                      return CircularProgressIndicator();
+                    }
+                    if (!snapshot.hasError && snapshot.hasData) {
+                      return Text(
+                        '${snapshot.data.point.toString()}pt',
+                        style: TextStyle(
+                          color: _theme.primaryColor,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 30,
+                        ),
+                      );
+                    }
+                    return Text('エラー');
+                  },
+                ),
+                currentAccountPicture: CircleAvatar(
+                  backgroundColor: Colors.white,
+                  backgroundImage: NetworkImage(_user.photoURL),
                 ),
               ),
               ListTile(
