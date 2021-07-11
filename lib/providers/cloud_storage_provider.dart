39d6b880f61f2746ba5428f667bc4bd89d074a5a
長政輝 <>
2021-07-11 19:32:14 +0900

    [clean] コメントを削除

--- lib/providers/cloud_storage_provider.dart
+++ lib/providers/cloud_storage_provider.dart
@@ -4,7 +4,6 @@ import 'package:firebase_storage/firebase_storage.dart';
 class CloudStorageProvider extends ChangeNotifier {
   Map<String, String> _store = {};
   Map<String, String> get store => _store;
-  // List<String> _loadings = [];
 
   void requestLoading(String path) async {
     if (_store.containsKey(path)) return; // 既に存在するなら離脱
