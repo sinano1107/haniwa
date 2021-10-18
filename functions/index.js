c633ee9d9d5f3678211544112315049645ea0105
長政輝 <>
2021-10-18 09:34:26 +0900

    [clean] 不要なfunctionを削除

--- functions/index.js
+++ functions/index.js
@@ -11,51 +11,6 @@ exports.questClear = questClear.questClear;
 exports.continuationCheck = continuationCheck.continuationCheck;
 exports.getTagData = get.getTagData;
 
-// /groups/:groupId/quest/:questIdが編集された際に
-// /groups/:groupId/tags/:tagId のコレクションからidが:questIdと等しいものを同じ値に編集する
-exports.editTagQuest = functions.region('asia-northeast1').firestore
-        .document(`${version}/groups/{groupId}/quests/{questId}`)
-        .onUpdate(async (change, context) => {
-    // アップデートされた値を取得
-    const groupId = context.params.groupId;
-    const questId = context.params.questId;
-    const data = change.after.data();
-    
-    // tagsコレクションからidが等しいものを探して編集
-    await admin.firestore().collection(`${version}/groups/${groupId}/tags`)
-        .where('id', '==', questId)
-        .get().then((querySnapShot) => {
-            querySnapShot.forEach(async (doc) => {
-                await doc.ref.update({
-                    'name': data.name,
-                    'point': data.point,
-                    'last': data.last,
-                });
-                functions.logger.log('editTagQuest', `${doc.id}を編集しました`);
-            });
-        });
-});
-
-// /groups/:groupId/quest/:questIdが削除された際に
-// /groups/:groupId/tags/:tagIdのコレクションからidが:questIdと等しいものを同じ値に編集する
-exports.deleteTagQuest = functions.region('asia-northeast1').firestore
-    .document(`${version}/groups/{groupId}/quests/{questId}`)
-    .onDelete(async (_, context) => {
-        const groupId = context.params.groupId;
-        const questId = context.params.questId;
-        // tagsコレクションからidが等しいものを探してidをnullにする
-        await admin.firestore().collection(`/groups/${groupId}/tags`)
-            .where('id', '==', questId)
-            .get().then((querySnapShot) => {
-                querySnapShot.forEach(async (doc) => {
-                    await doc.ref.update({
-                        'id': null,
-                    });
-                    functions.logger.log('deleteTagQuest', `${doc.id}のidをnullに設定しました`);
-                });
-            });
-    });
-
 // テスト関数
 exports.test = functions.region('asia-northeast1').https.onRequest(async (_, res) => {
     const date = new Date();
